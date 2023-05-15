1ec01434493de002938aedc402cf27f26498c9d5
mjmartinezstudium <mjmartinezstudium@gmail.com>
2023-05-15 13:18:45 +0200

    WindowListener

---
+++ src/es/studium/Slot/MenuPrincipal.java
@@ -0,0 +1,14 @@
+package es.studium.Slot;
+
+import java.awt.Frame;
+
+public class MenuPrincipal extends Frame
+{
+	private static final long serialVersionUID = 1L;
+	
+	MenuPrincipal()
+	{
+		setSize(400,200);
+		setVisible(true);
+	}
+}
---
+++ src/es/studium/Slot/Modelo.java
@@ -0,0 +1,6 @@
+package es.studium.Slot;
+
+public class Modelo
+{
+
+}
