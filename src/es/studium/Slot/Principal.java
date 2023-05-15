4fc82a41280220b6594d8466e76cd3d55762adcc
mjmartinezstudium <mjmartinezstudium@gmail.com>
2023-05-15 13:09:55 +0200

    Clase Principal, código

--- src/es/studium/Slot/Principal.java
+++ src/es/studium/Slot/Principal.java
@@ -2,11 +2,10 @@ package es.studium.Slot;
 
 public class Principal
 {
-
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		Modelo modelo = new Modelo();
+		MenuPrincipal menuPrincipal = new MenuPrincipal();
+		new Controlador(modelo, menuPrincipal);
 	}
-
 }
